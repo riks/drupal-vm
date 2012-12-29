cfd385dba2b15942704ef261f56e0f1e2a47a1be
Bussmeyer <thomas@gretel-2.local>
2012-12-29 10:04:42 +0100

    Open port 80.

--- puppet/vagrant-manifests/drupal_web.pp
+++ puppet/vagrant-manifests/drupal_web.pp
@@ -1,1 +1,14 @@
+# TODO: Search for a iptables/more generic module
+# https://github.com/puppetlabs/puppetlabs-firewall
+# http://geek.jasonhancock.com/2011/10/11/managing-iptables-firewalls-with-puppet/
+exec { "open port 80":
+    command => "iptables -I INPUT -p tcp --dport 80 -j ACCEPT",
+    path    => "/sbin/",
+}
+exec { "open port 80 save":
+    command => "service iptables save",
+    path    => "/sbin/",
+
+}
+
 include apache
