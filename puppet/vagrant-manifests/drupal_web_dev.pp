87ba89947081ded2098b8cd352b46d9b6c83e146
Bussmeyer <thomas@gretel-2.local>
2013-01-20 14:08:29 +0100

    Rename file

---
+++ puppet/vagrant-manifests/drupal_web_dev.pp
@@ -0,0 +1,28 @@
+class { 'iptables': }
+
+class { "apache": }
+apache::vhost { 'localhost':
+  docroot  => '/vagrant/www/localhost/public_html',
+}
+apache::vhost { 'phpinfo':
+  docroot  => '/vagrant/www/phpinfo/public_html',
+}
+
+class { "mysql":
+  root_password => 'password',
+}
+mysql::grant { 'drupal':
+  mysql_privileges => 'ALL',
+  mysql_password => 'password',
+  mysql_db => 'drupal',
+  mysql_user => 'drupal',
+  mysql_host => 'localhost',
+}
+
+class { 'php': }
+php::module { "mysql": }
+php::module { "dom": }
+php::module { "gd": }
+php::module { "mbstring": }
+
+#include drush
