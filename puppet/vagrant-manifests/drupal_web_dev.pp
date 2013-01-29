73cc8d2c1d7c320dbc24a7f9c29dbe0ca484132e
Bussmeyer <thomas.bussmeyer@gmx.de>
2013-01-29 21:49:48 +0100

    Include drupal_web config class. Remove duplicates.

--- puppet/vagrant-manifests/drupal_web_dev.pp
+++ puppet/vagrant-manifests/drupal_web_dev.pp
@@ -1,14 +1,5 @@
-# update os (yum update)
+include drupal_web
 
-#class { "iptables": }
-exec { "open port 80":
-  command => "iptables -I INPUT -p tcp --dport 80 -j ACCEPT",
-  path    => "/sbin/",
-}
-exec { "open port 80 save":
-  command => "service iptables save",
-  path    => "/sbin/",
-}
 exec { "open port 9999":
   command => "iptables -I INPUT -p tcp --dport 9999 -j ACCEPT",
   path    => "/sbin/",
@@ -18,48 +9,10 @@ exec { "open port 9999 save":
   path    => "/sbin/",
 }
 
-class { "apache": }
-apache::vhost { 'localhost':
-  docroot  => '/vagrant/www/localhost/public_html',
-}
 apache::vhost { 'phpMyAdmin':
   port => 9999,
   docroot  => '/vagrant/www/localhost/public_html',
 }
 
-#class { "mysql":
-#  root_password => 'password',
-#}
-#mysql::grant { "drupal":
-#  mysql_privileges => 'ALL',
-#  mysql_password => 'password',
-#  mysql_db => 'drupal',
-#  mysql_user => 'drupal',
-#  mysql_host => 'localhost',
-#}
-
-#class { "php": }
-#php::module { "mysql": }
-#php::module { "xml": }
-#php::module { "gd": }
-#php::module { "mbstring": }
-# memory_limit = 256M
-
-#class { "pear": }
-#pear::package { "PEAR": }
-#pear::package { "Console_Table": }
-#pear::package { "drush":
-  #version => "5.8.0",
-  #repository => "pear.drush.org",
-  #require => Pear::Package["PEAR"],
-#}
-
 # http://www.krizna.com/centos/how-install-phpmyadmin-centos-6/
 # phpMyAdmin
-
-#cron { "drupal-cron-localhost":
-#  command => "wget -O - -q -t 1 http://localhost/cron.php",
-#  hour => inline_template("<%= name.hash % 24 %>"),
-#  minute => "00",
-#}
-
